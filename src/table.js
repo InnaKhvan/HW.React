16ad112daa8c2cf524b45052d9600d4c95d9fa9d
Inna Khvan <in.khvan@bk.ru>
2023-02-03 17:21:52 +0900

    changes file

--- src/table.js
+++ src/table.js
@@ -2,30 +2,20 @@ import food from './food.json'
 
 export const Table = () => {
     return (
-        <div className="container">
+        <table className="table-bordered">
+            <caption>Меню</caption>
+            {food.map((item, index) => (
+                <tr className='line-data' data-index={index}>
 
-            <table className="table-bordered">
-                <caption>Меню</caption>
-                <tr className='line-main'>
-                    <th>Номер</th>
-                    <th>Наименование</th>
-                    <th>Ингридиенты</th>
-                    <th>Цена</th>
-                    <th></th>
+                    <td className='Name-food'>{item.name}</td>
+                    <td className='Ing-food'>Ингридиенты: {item.ingredients.map(name => (
+                        <li>
+                            {name}
+                        </li>))}</td>
+                    <td className='Price'>Цена: {item.price}</td>
+                    <td><img src={item.link} className="Picture" alt="picture" /></td>
                 </tr>
-                {food.map((item, index) => (
-                    <tr className='line-data' data-index={index}>
-                        <td>{item.id}</td>
-                        <td>{item.name}</td>
-                        <td>{item.ingredients.map(name => (
-                            <li>
-                                {name}
-                            </li>))}</td>
-                        <td>{item.price}</td>
-                        <td><img src = {item.link}/></td>
-                    </tr>
-                ))}
-            </table>
-        </div>
+            ))}
+        </table>
     );
 }
