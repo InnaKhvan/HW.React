bde73564c122b730cb5750eac4e9628dbe6a4d85
Inna Khvan <in.khvan@bk.ru>
2023-02-12 19:43:41 +0900

    changing the structure of the project

--- src/components/basket/amount.js
+++ src/components/basket/amount.js
@@ -1,4 +1,26 @@
 
-const Amount = ({item}) => {
-    return 
+const Amound = ({ items }) => {
+    return (<>
+        {items.map((item, index) => 
+        (<div className="flex bg-cyan-300 p-2 m-2 rounded-lg" key={`card-${index}` }>
+         {item.name}</div>))}
+
+            <div className="flex bg-cyan-300 p-2 m-2 rounded-lg"><p>Итого: </p>{
+            items.reduce((sum, item) => {
+                sum += item.price;
+                return sum;
+            }, 0)}</div>
+        </>)
 }
+
+export default Amound;
+
+
+
+let [qty, sum] = reduce((arr,item) => {
+    arr[0] += item.qty;
+    arr[1] += item.qty * item.food.price;
+  }),
+  [0, 0];
+
+console.log(arr[1]);
