b49f771931feeb634827d74fdf93e1188ba9bbce
Inna Khvan <in.khvan@bk.ru>
2023-02-18 12:41:00 +0900

    add redux

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import Header from "./components/header/header";
 import { FoodList } from "./components/foodList/foodList";
 import food from "./components/foodList/food.json";
 import Basket from "./components/basket/basket";
-import { useState } from "react";
 
 function App() {
-  const [basketItems, setBasketItems] = useState([]);
-  const onItemSelect = (item) => {
-    console.log(item);
-    setBasketItems([...basketItems, item]);
-  };
+ 
   return (
     <div className="App">
       <Header/>
       <div className="flex flex-row">
         <div className="flex basis-3/4 flex-col md:flex-row">
-          <FoodList items={food} onSelect={onItemSelect} />
+          <FoodList items={food} />
         </div>
         <div className="flex flex-col basis-1/5 bg-cyan-200 p-2 m-2 rounded-lg">
           Ваш выбор
