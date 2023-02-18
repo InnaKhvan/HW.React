843557e839d260ff3a4599e0c5672d2e38ac8841
Inna Khvan <in.khvan@bk.ru>
2023-02-18 12:42:19 +0900

    chage file basket.js

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -6,7 +6,6 @@ const Basket = () => {
         {items.map((item, index) => 
         (<div className="flex bg-cyan-300 p-2 m-2 rounded-lg" key={`card-${index}` }>
          {item.name}</div>))}
-
             <div className="flex bg-cyan-300 p-2 m-2 rounded-lg"><p>Итого: </p>{
             items.reduce((sum, item) => {
                 sum += item.price;
